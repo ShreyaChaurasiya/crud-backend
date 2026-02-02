86207241bfe706eef6c93f292e45b1fdff96add3
ShreyaChaurasiya <chaurasiya.shreyaa@gmail.com>
2026-02-02 17:12:27 +0530

    Fix CORS issue for Vercel frontend

--- src/main/java/com/crudapplication/crud/controller/UserController.java
+++ src/main/java/com/crudapplication/crud/controller/UserController.java
@@ -10,43 +10,33 @@ import com.crudapplication.crud.services.UserService;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(
-	    origins = {
-	        "http://localhost:3000",
-	        "https://crud-frontend-murex.vercel.app"
-	    }
-	)
+@CrossOrigin(origins = "*")   // ✅ FIXED
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    // CREATE
     @PostMapping
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
-    // READ ALL
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
-    // READ ONE
     @GetMapping("/{userId}")
     public User getUser(@PathVariable Long userId) {
         return userService.getUser(userId);
     }
 
-    // UPDATE
     @PutMapping("/{userId}")
     public User updateUser(@PathVariable Long userId, @RequestBody User user) {
         user.setId(userId);
         return userService.updateUser(user);
     }
 
-    // DELETE
     @DeleteMapping("/{userId}")
     public User deleteUser(@PathVariable Long userId) {
         return userService.deleteUser(userId);
